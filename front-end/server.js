749de7c15b83586f0d813a01e95008e851485b60
domenicogaeni <domenicogaeni@gmail.com>
2022-01-05 17:40:57 +0100

    :rocket: Change node port.

--- front-end/server.js
+++ front-end/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 80;
 
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
