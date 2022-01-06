b53571f2205764d26f4c02add49443bdbc5dc98c
Fabio <febpalazzi@gmail.com>
2022-01-06 15:36:43 +0100

    Add request of boght and sold books

--- front-end/js/API/load_history.js
+++ front-end/js/API/load_history.js
@@ -1,3 +1,68 @@
-$(document).ready(function(){
-    //chiamata per caricare i libri
+$(document).ready(function (){
+    $.ajax({
+        type: 'GET',
+        url: 'https://ingegneria-software.herokuapp.com/public/order',
+        crossDomain: true,
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        beforeSend: function(xhr) {
+            if (localStorage.token) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.token);
+            }
+        },
+        success: function(data) {
+            //story of bought book section
+            $("#books_search").empty();
+            var data = data.data.bought_books;
+            var html_book = "";
+            for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            console.log(element.title);
+            if(index%4==0)
+                html_book += '<div class="row">';
+            html_book += '<div class="col-xl-3 col-md-6 mb-4">\
+                                <div class="card">\
+                                    <div class="card-body">\
+                                        <h5 class="card-title">' + element.book_info.title + '</h5>\
+                                        <h7 class="card-subtitle mb-2 text-muted">' + element.book_info.authors + ' - ' + element.book_info.gender + '</h7>\
+                                        <h6 class="card-subtitle mb-2 text-muted">Prezzo: ' + element.book_info.price + '€</h6>\
+                                        <h8 class="card-subtitle mb-2 text-muted"></h8>\
+                                        <p class="card-text">Comprato da: ' + element.book_info.reseller_info.name + ' ' + element.book_info.reseller_info.last_name + '</p>\
+                                    </div>\
+                                </div>\
+                            </div>';
+            if(index%4==3 || index == data.length-1)
+                html_book += '</div>';
+            }
+            $("#books_search").append(html_book);
+
+            //story of sold book section <campi mancano>
+            $("#books_sell").empty();
+            var data = data.data.bought_books;
+            var html_book = "";
+            for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            console.log(element.title);
+            if(index%4==0)
+                html_book += '<div class="row">';
+            html_book += '<div class="col-xl-3 col-md-6 mb-4">\
+                                <div class="card">\
+                                    <div class="card-body">\
+                                        <h5 class="card-title">' + element.book_info.title + '</h5>\
+                                        <h7 class="card-subtitle mb-2 text-muted">' + element.book_info.authors + ' - ' + element.book_info.gender + '</h7>\
+                                        <h6 class="card-subtitle mb-2 text-muted">Prezzo: ' + element.book_info.price + '€</h6>\
+                                        <h8 class="card-subtitle mb-2 text-muted"></h8>\
+                                        <p class="card-text">Comprato da: ' + element.book_info.reseller_info.name + ' ' + element.book_info.reseller_info.last_name + '</p>\
+                                    </div>\
+                                </div>\
+                            </div>';
+            if(index%4==3 || index == data.length-1)
+                html_book += '</div>';
+            }
+            $("#books_sell").append(html_book);
+        },
+        error: function(data) {
+            alert("Error");
+        }
+        });
 })
