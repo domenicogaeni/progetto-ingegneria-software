c61cf2d2940ecb84027851a02ebb68a13389d79d
Fabio <febpalazzi@gmail.com>
2022-01-13 11:49:04 +0100

    :pick: Adding evaluate results

--- front-end/public/js/API/search_book.js
+++ front-end/public/js/API/search_book.js
@@ -21,36 +21,45 @@ $(document).ready(function () {
           const element = data[index];
           if (index % 4 == 0) html_book += '<div class="row">';
           html_book +=
-            '<div class="col-xl-3 col-md-6 mb-4">\
-                                    <div class="card">\
-                                        <div class="card-body">\
-                                            <h5 class="card-title">' +
-            element.title +
-            '</h5>\
-            <h7 class="card-subtitle mb-2 text-muted"> ISBN: ' +
-            element.isbn +
-            '</h7><br/>\
-                                            <h7 class="card-subtitle mb-2 text-muted">' +
-            element.authors +
-            " - " +
-            element.gender +
-            '</h7>\
-                                            <h6 class="card-subtitle mb-2 text-muted">Prezzo: ' +
-            element.price +
-            '€</h6>\
-                                            <h8 class="card-subtitle mb-2 text-muted"></h8>\
-                                            <p class="card-text">';
+          '<div class="col-xl-3 col-md-6 mb-4">\
+            <div class="card">\
+                <div class="card-body">\
+                    <h5 class="card-title">' +
+          element.title +
+          '</h5>\
+          <h7 class="card-subtitle mb-2 text-muted"> ISBN: ' +
+          element.isbn +
+          '</h7><br/>\
+                    <h7 class="card-subtitle mb-2 text-muted">' +
+          element.authors +
+          " - " +
+          element.gender +
+          '</h7>\
+                    <h6 class="card-subtitle mb-2 text-muted">Prezzo: ' +
+          element.price +
+          '€</h6>\
+                    <h8 class="card-subtitle mb-2 text-muted"></h8>\
+                    <p class="card-text">';
           if (element.description != null) html_book += element.description;
           html_book +=
-            '</p>\
-                                            <a class="btn btn-primary buttonBuy" onclick="openModal(' +
-            element.price +
-            "," +
-            element.id +
-            ')">Compra</a>\
-                                        </div>\
-                                    </div>\
-                                </div>';
+          '</p>';
+          if(element.average_vote!=null){
+            html_book += '<section class="p-4 d-flex justify-content-center text-center w-100">';
+            for(var i=0; i<5; i++){
+              if(i+0.5<=average_vote)
+                html_book+='<i class="fa-star fa-sm text-primary fas active"</i>';
+              else
+                html_book+='<i class="far fa-star fa-sm text-primary"</i>';
+            }
+          }
+          html_book += '<a class="btn btn-primary buttonBuy" onclick="openModal(' +
+          element.price +
+          "," +
+          element.id +
+          ')">Compra</a>\
+                </div>\
+            </div>\
+          </div>';
           if (index % 4 == 3 || index == data.length - 1) html_book += "</div>";
         }
         $("#books_searched").append(html_book);
